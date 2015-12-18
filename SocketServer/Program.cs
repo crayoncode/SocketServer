d9bf1594566505d4823dfbe8a05ca9d7aead6adc
ZhaoYang <492118114@qq.com>
2015-12-18 15:05:29 +0800

    服务端Demo

---
+++ SocketServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int port = 1514;
+            string host = "127.0.0.1";
+            //创建终结点
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip,port);
+           
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(ipe);
+            s.Listen(0);
+            Console.WriteLine("等待客户端连接");
+
+            //接受到Client连接，为此连接建立新的Socket，并接受消息
+            Socket temp = s.Accept();   //为新建立的连接创建新的Socket
+            Console.WriteLine("建立连接");
+            string recvStr = "";
+            byte[] recvBytes = new byte[1024];
+            int bytes;
+            bytes = temp.Receive(recvBytes, recvBytes.Length, 0); //从客户端接受消息
+            recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
+
+            //给Client端返回信息
+            Console.WriteLine("server get message:{0}", recvStr);    //把客户端传来的信息显示出来
+            string sendStr = "ok!Client send message successful!";
+            byte[] bs = Encoding.ASCII.GetBytes(sendStr);
+            temp.Send(bs, bs.Length, 0);  //返回信息给客户端
+            temp.Close();
+            s.Close();
+            Console.ReadLine();
+ 
+        }
+    }
+}
---
+++ SocketUDPServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace SocketUDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            int port = 1514;
+            string host = "127.0.0.1";
+            byte[] recvBytes = new byte[1024];
+            //创建终结点
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            s.Bind(ipe);
+           
+            Console.WriteLine("等待客户端连接");
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint remote = (EndPoint)(sender);
+            Console.WriteLine("建立连接");
+            recv = s.ReceiveFrom(recvBytes, ref remote);
+            Console.WriteLine("Message  recevie from {0}",remote.ToString());
+            string str = System.Text.Encoding.ASCII.GetString(recvBytes, 0, recv);
+            Console.WriteLine("Messsage:{0}",str);
+            str = "Hello Client!";
+
+            recvBytes = System.Text.Encoding.ASCII.GetBytes(str);
+            //Thread.Sleep(1000);  
+            //发送到客户端
+            s.SendTo(recvBytes,remote);
+           
+            Console.ReadLine();
+        }
+    }
+}
